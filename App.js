8b3f7322be9eabe645cb72f1917bd158cdd98d52
Victor Martins <victor.martins.dev5@gmail.com>
2022-12-07 10:59:47 -0300

    Parte Visual
    Parte visual da calculadora semipronta

--- App.js
+++ App.js
@@ -1,12 +1,105 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
+
+var height = Dimensions.get("window").height;
+var width = Dimensions.get("window").width;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <View style={styles.calculadora}>
+        <View
+          style={styles.visor}>
+          <Text style={styles.texto}>80 x 50</Text>
+          <Text style={[styles.texto, {marginTop: 20, color: "white"}]}>4000</Text>
+        </View>
+
+        <View style={styles.botoes_grupo}>
+          <TouchableOpacity style={styles.botoes}>
+            <Text style={styles.botoes_texto}>C</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.botoes}>
+            <Text style={styles.botoes_texto}>( )</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.botoes}>
+            <Text style={styles.botoes_texto}>%</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.botoes}>
+            <Text style={styles.botoes_texto}>÷</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botoes}>
+            <Text style={styles.botoes_texto}>7</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botoes}>
+            <Text style={styles.botoes_texto}>8</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botoes}>
+            <Text style={styles.botoes_texto}>9</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botoes}>
+            <Text style={styles.botoes_texto}>X</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botoes}>
+            <Text style={styles.botoes_texto}>4</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botoes}>
+            <Text style={styles.botoes_texto}>5</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botoes}>
+            <Text style={styles.botoes_texto}>6</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botoes}>
+            <Text style={styles.botoes_texto}>-</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botoes}>
+            <Text style={styles.botoes_texto}>1</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botoes}>
+            <Text style={styles.botoes_texto}>2</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botoes}>
+            <Text style={styles.botoes_texto}>3</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botoes}>
+            <Text style={styles.botoes_texto}>+</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botoes}>
+            <Text style={styles.botoes_texto}>+/-</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botoes}>
+            <Text style={styles.botoes_texto}>0</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botoes}>
+            <Text style={styles.botoes_texto}>,</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botoes}>
+            <Text style={styles.botoes_texto}>=</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,8 +107,55 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#222222',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  calculadora: {
+    flex: 1,
+    marginTop: height * .1,
+    justifyContent: "flex-start",
+    flexDirection: "column"
+  },
+
+  visor: {
+    height: height * .4,
+    width: width * .86,
+    borderBottomColor: "#323232",
+    borderBottomWidth: 2,
+    marginHorizontal: "7%"
+  },
+
+  texto: {
+    fontSize: 30,
+    color: "#ffa500",
+    fontWeight: "500",
+    textAlign: "right",
+  },
+
+  botoes_grupo: {
+    height: height * .6,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingTop: "5%",
+    paddingHorizontal: "10%",
+    flexWrap: "wrap",
+  },
+
+  botoes: {
+    width: (height * .7) * .13,
+    backgroundColor: "#383838",
+    borderRadius: 100,
+    height: (height * .7) * .13,
+    marginBottom: 10,
+    justifyContent: "center"
+  },
+
+  botoes_texto: {
+    fontSize: 25,
+    color: "white",
+    fontWeight: "500",
+    textAlign: "center",
+  }
 });
